ed14c868bdbf993dbe06ceee5c6b26c4e411ce22
enava3 <navaem@gmail.com>
2019-11-14 14:04:42 -0500

    login screen created

--- src/Login/index.js
+++ src/Login/index.js
@@ -5,16 +5,20 @@ import { Redirect } from 'react-router-dom'
 class Login extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { authenticated: null };
+    this.state = { 
+      authenticated: null,
+      username: "",
+      password: "",
+    };
     this.checkAuthentication = this.checkAuthentication.bind(this);
     this.login = this.login.bind(this);
   }
  
   async checkAuthentication() {
-    const authenticated = await this.props.auth.isAuthenticated();
-    if (authenticated !== this.state.authenticated) {
-      this.setState({ authenticated });
-    }
+    // const authenticated = await this.props.auth.isAuthenticated();
+    // if (authenticated !== this.state.authenticated) {
+    //   this.setState({ authenticated });
+    // }
   }
  
   async componentDidMount() {
@@ -22,7 +26,7 @@ class Login extends React.Component {
   }
  
   async login(e) {
-    this.props.auth.login('/home');
+    this.props.login('/home');
   }
  
   render() {
@@ -31,11 +35,33 @@ class Login extends React.Component {
     } else {
       return (
         <div style={{height: '100vh', display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
-          <Button variant="contained" color="primary" onClick={this.login}>Login with Okta</Button>
+          <form>
+            <h1>Login</h1>
+            <label>Username</label>
+            <input
+              name='username'
+              placeholder='Username'
+              value={this.state.username}
+              onChange={this.handleChange}
+              /><br/>
+
+            <label>Password</label>
+            <input
+              type='password'
+              name='password'
+              placeholder='Password'
+              value={this.state.password}
+              onChange={this.handleChange}
+              /><br/>
+
+            <Button variant="contained" color="primary" onClick={this.login}>Login</Button>
+            <Button variant="contained" color="primary" onClick={this.login}>Register</Button>
+            
+          </form>
         </div>
       )
     }
   }
  }
  
- export default withAuth(Login);
+ export default Login;
