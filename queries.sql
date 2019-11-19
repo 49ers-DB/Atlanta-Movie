170e26b33e49c55ab517761bc1612cbb4f6269d7
indymichio23 <57683575+indymichio23@users.noreply.github.com>
2019-11-18 22:05:37 -0500

    Add files via upload

--- queries.sql
+++ queries.sql
@@ -62,13 +62,39 @@ values (i_thName, i_comName, i_capacity, i_thStreet, i_thCity, i_thState, i_thZi
 insert into Movie (movName, movReleaseDate, duration)
 values (i_movName, i_movReleaseDate, i_duration);
 
--- Schedule Movie
+-- Schedule Movie: I literally don't know how to do this
 
-select movName as 'Name', movReleaseDate as 'Release Date' from Movie;
+select movName, movReleaseDate from Movie
+
 insert into MoviePlay (thName, comName, movName, movReleaseDate, movPlayDate)
     values (i_thName, i_comName, i_movName, i_movReleaseDate, i_movPlayDate)
-    where Manager.comName = i_comName and Manager. --im confused
 
 -- Company Detail
 
-select
+    -- Admin view company detail (Employee)
+select firstname, lastname from user inner join manager on user.username = manager.username where i_comName = manager.comName
+
+    -- Admin view company detail (Theater)
+select thName, manUsername, thCity, thState, capacity from theater where i_comName = theater.comName
+
+-- Manager Filter Theater (Manager Theater Overview) : Literally don't think this is right but I tried
+
+DROP PROCEDURE IF EXISTS manager_filter_th;
+DELIMITER $$
+CREATE PROCEDURE `manager_filter_th`(IN i_manUsername VARCHAR(50), IN i_movName VARCHAR(50), IN i_minMovDuration INT, IN i_maxMovDuration INT, IN i_minMovReleaseDate DATE, IN i_maxMovReleaseDate DATE, IN i_minMovPlayDate DATE, IN i_maxMovPlayDate DATE, IN i_includeNotPlayed BOOLEAN)
+BEGIN
+    DROP TABLE IF EXISTS ManagerFilterTh;
+    CREATE TABLE ManagerFilterTh
+    SELECT movie.movName, movie.duration, movie.movReleaseDate, MoviePlay.movPlayDate
+    FROM Movie inner join MoviePlay on movie.MovName = MoviePlay.MovName AND movie.movReleaseDate = MoviePlay.movReleaseDate
+    WHERE (movie.movName = i_movName) AND -- trying to figure out how to use $$ to make it to where someone can put in part of a movie name and get results of maybe a keyword or something??
+        (movie.duration >= i_minMovDuration OR movie.duration <= i_maxMovDuration OR i_minMovDuration = "ALL" OR i_maxMovDuration = "ALL") AND
+        (movie.movReleaseDate >= i_minMovReleaseDate OR movie.movReleaseDate <= i_maxMovReleaseDate OR i_minMovReleaseDate = "ALL" OR i_maxMovDuration = "ALL") AND
+        (MoviePlay.movPlayDate >= i_minMovPlayDate OR MoviePlay.movPlayDate <= i_maxMovPlayDate OR i_minMovPlayDate = "ALL" OR i_maxMovPlayDate = "ALL") AND
+        (MoviePlay.movPlayDate >= i_includeNotPlayed);
+    END$$
+    DELIMITER ;
+
+
+
+
