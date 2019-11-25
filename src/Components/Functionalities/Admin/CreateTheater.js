0d0d8c14cb2a8fcaafb221f69186dcba7c73b5b6
navaem <navaem@gmail.com>
2019-11-25 17:01:04 -0500

    created outline of CreateTheater

--- src/Components/Functionalities/Admin/CreateTheater.js
+++ src/Components/Functionalities/Admin/CreateTheater.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import APIClient from "../../../apiClient"
+import Select from 'react-select'
 
 import "../Functionality.css"
 
@@ -37,44 +38,47 @@ export default class CreateTheater extends Component {
       <div className="main">
         <div className="card visitHistoryCard">
           <div className="card-header">
-            <h2>Create Movie</h2>
+            <h2>Create Theater</h2>
           </div>
-          <div className="card visitHistoryTableCard">
-          <div className="functionalities-table">
-          <table className="table">
-            <thead>
-              <tr>
-                <th scope="col">Movie</th>
-                <th scope="col">Theater</th>
-                <th scope="col">Company</th>
-                <th scope="col">Card#</th>
-                <th scope="col">View Date</th>
-              </tr>
-            </thead>
-            <tbody>
+          <div className="card-body">
+            <div className="row">
+              <div className="form-inline functionalities-form-row">
+                <label>Name</label>
+                <input className="form-control"
+                />
+                <div className="col"></div>
+                <label>Company</label>
+                <Select className="functionalities-select"
+                />
+              </div>
               
-                {this.state.rowData.map( (row) => {
-                  return (
-                    <tr key={this.state.rowData.indexOf(row)}>
-                      <td>{row[0]}</td>
-                      <td>{row[1]}</td>
-                      <td>{row[2]}</td>
-                      <td>{row[3]}</td>
-                      <td>{row[4]}</td>
-                    </tr>
-                  );
-                })}
-    
-            </tbody>
-          </table>
-          </div>
+            </div>
+            <div className="row functionalities-form-row form-inline">
+              <label>Street Address</label>
+              <input className="form-control col-9"/>
+            </div>
+            <div className="row functionalities-form-row form-inline">
+              <label className="col-1">City</label>
+              <input className="form-control"></input>
+              <label className="col-1">State</label>
+              <Select className="col-2"
+              />
+              <label className="col-1">Zipcode</label>
+              <input className="form-control col-2"/>
+            </div>
+            <div className="row functionalities-form-row form-inline">
+              <label>Capacity</label>
+              <input className="form-control"/>
+              <label>Manager</label>
+              <Select className="functionalities-select"
+              />
+            </div>
 
           </div>
           
           <div className="row">
-            <div className="col-12">
-              <a className="btn btn-primary" href="/menu">Back</a>
-            </div>
+            <a className="btn btn-primary" href="/auth/Manage-Company">Back</a>
+            <button className="btn btn-primary">Create</button>
           </div>
           
 
