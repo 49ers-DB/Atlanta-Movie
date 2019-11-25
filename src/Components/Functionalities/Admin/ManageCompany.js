25f277a1e2f965f5d92ffa33611707250cf30dd3
navaem <navaem@gmail.com>
2019-11-25 11:58:15 -0500

    created inputs for manageCompany

--- src/Components/Functionalities/Admin/ManageCompany.js
+++ src/Components/Functionalities/Admin/ManageCompany.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import APIClient from "../../../apiClient"
+import Select from 'react-select'
 
 import "../Functionality.css"
 
@@ -8,7 +9,7 @@ export default class ManageCompany extends Component {
     super(props)
     this.state = {
       apiClient: null,
-      rowData: [[],[],[],[],[],[]],
+      rowData: [],
     }
     
     var accessToken = localStorage.getItem("accessToken")
@@ -35,39 +36,70 @@ export default class ManageCompany extends Component {
   render () {
     return (
       <div className="main">
-        <div className="card visitHistoryCard">
+        <div className="card">
           <div className="card-header">
             <h2>Manage Company</h2>
           </div>
-          <div className="card visitHistoryTableCard">
-          <div className="functionalities-table">
-          <table className="table">
-            <thead>
-              <tr>
-                <th scope="col">Movie</th>
-                <th scope="col">Theater</th>
-                <th scope="col">Company</th>
-                <th scope="col">Card#</th>
-                <th scope="col">View Date</th>
-              </tr>
-            </thead>
-            <tbody>
-              
-                {this.state.rowData.map( (row) => {
-                  return (
-                    <tr key={this.state.rowData.indexOf(row)}>
-                      <td>{row[0]}</td>
-                      <td>{row[1]}</td>
-                      <td>{row[2]}</td>
-                      <td>{row[3]}</td>
-                      <td>{row[4]}</td>
-                    </tr>
-                  );
-                })}
-    
-            </tbody>
-          </table>
-          </div>
+          <div className="card-body">
+            <div className="row">
+              <div className="form-group form-inline functionalities-form-row">
+                <label>Name</label>
+                <Select className="functionalities-select"
+                />
+                <label>Num. Cities Covered</label>
+                <input className="form-control col-1"/>
+                <div>--</div>
+                <input className="form-control col-1"/>
+              </div>
+
+            </div>
+            <div className="row">
+              <div className="form-group form-inline functionalities-form-row">
+                <label>Num. Theaters</label>
+                <input className="form-control col-1"/>
+                <div>--</div>
+                <input className="form-control col-1"/>
+                <label>Num. Employees</label>
+                <input className="form-control col-1"/>
+                <div>--</div>
+                <input className="form-control col-1"/>
+              </div>
+            </div>
+            <div className="row">
+              <button className="btn btn-primary col-3">Filter</button>
+              <div className="col-3"></div>
+              <a className="btn btn-primary col-2" href="/">Create Theater</a>
+              <button className="btn btn-primary col-2" href="/">Detail</button>
+            </div>
+            
+            <div className="functionalities-table visitHistoryTableCard">
+            <table className="table">
+              <thead>
+                <tr>
+                  <th scope="col">Selected</th>
+                  <th scope="col">Name</th>
+                  <th scope="col">Num. Cities Covered</th>
+                  <th scope="col">Num. Theaters</th>
+                  <th scope="col">Num. Employees</th>
+                </tr>
+              </thead>
+              <tbody>
+                
+                  {this.state.rowData.map( (row) => {
+                    return (
+                      <tr key={this.state.rowData.indexOf(row)}>
+                        <td>{row[0]}</td>
+                        <td>{row[1]}</td>
+                        <td>{row[2]}</td>
+                        <td>{row[3]}</td>
+                        <td>{row[4]}</td>
+                      </tr>
+                    );
+                  })}
+      
+              </tbody>
+            </table>
+            </div>
 
           </div>
           
