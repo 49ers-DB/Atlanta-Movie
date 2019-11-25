007996c2a648041a7d606dd730a0b95fe2d6337d
navaem <navaem@gmail.com>
2019-11-25 09:13:07 -0500

    added all of the onChange handlers

--- src/Components/Functionalities/Manager/TheaterOverview.js
+++ src/Components/Functionalities/Manager/TheaterOverview.js
@@ -18,7 +18,13 @@ export default class TheaterOverview extends Component {
       moviePlayDate2: null,
       includeNotPlayed: false
     }
+    this.setMovieName = this.setMovieName.bind(this)
     
+    this.handleFilter(new Event(""))
+  }
+
+  handleFilter(event) {
+    event.preventDefault()
     var accessToken = localStorage.getItem("accessToken")
     
     if (accessToken) {
@@ -27,8 +33,37 @@ export default class TheaterOverview extends Component {
     }
   }
 
+  setMovieName(event) {
+    this.setState({movieName: event.target.value})
+  }
 
+  setMovieDur1(event) {
+    this.setState({movieDuration1: event.target.value})
+  }
 
+  setMovieDur2(event) {
+    this.setState({movieDuration2: event.target.value})
+  }
+
+  setMovRelDate1(movieReleaseDate1) {
+    this.setState({movieReleaseDate1})
+  }
+
+  setMovRelDate2(movieReleaseDate2) {
+    this.setState({movieReleaseDate2})
+  }
+
+  setMovPlayDate1(moviePlayDate1) {
+    this.setState({moviePlayDate1})
+  }
+
+  setMovPlayDate2(moviePlayDate2) {
+    this.setState({moviePlayDate2})
+  }
+
+  setInclude(event) {
+    this.setState({includeNotPlayed: event.target.value})
+  }
 
   render () {
     return (
@@ -41,33 +76,53 @@ export default class TheaterOverview extends Component {
             <div className="row">
               <div className="form-group form-inline functionalities-form-row col">
                 <label>Movie Name</label>
-                <input className="form-control"></input>
-                <label>Morning Duration</label>
-                <input className="form-control col-1"></input>
+                <input className="form-control" onChange={this.setMovieName} value={this.state.movieName}></input>
+                <label>Movie Duration</label>
+                <input className="form-control col-1" onChange={this.setMovieDur1} value={this.state.movieDuration1}></input>
                 <div>--</div>
-                <input className="form-control col-1"></input>
+                <input className="form-control col-1" onChange={this.setMovieDur2} value={this.state.movieDuration2}></input>
               </div>
             </div>
             <div className="row">
               <div className="form-group form-inline functionalities-form-row col-9">
                 <label>Movie Release Date</label>
-                <DatePicker className="form-control"/>
+                <DatePicker className="form-control"
+                value={this.state.movieReleaseDate1}
+                selected={this.state.movieReleaseDate1}
+                onChange={this.setMovRelDate1}
+                />
                 <div className="col-1">--</div>
-                <DatePicker className="form-control"/>
+                <DatePicker className="form-control"
+                value={this.state.movieReleaseDate2}
+                selected={this.state.movieReleaseDate2}
+                onChange={this.setMovRelDate2}
+                />
               </div>
               <div className="form-group form-inline functionalities-form-row col-9">
                 <label>Movie Play Date</label>
-                <DatePicker className="form-control"/>
+                <DatePicker className="form-control"
+                value={this.state.moviePlayDate1}
+                selected={this.state.moviePlayDate1}
+                onChange={this.setMovPlayDate1}
+                />
                 <div className="col-1">--</div>
-                <DatePicker className="form-control"/>
+                <DatePicker className="form-control"
+                value={this.state.moviePlayDate2}
+                selected={this.state.moviePlayDate2}
+                onChange={this.setMovPlayDate2}
+                />
               </div>
             </div>
             <div>
-              <div>
-                
-                <input type="checkbox"></input>
-                <label> Only include not played movies</label>
-              </div>
+              
+              <input type="checkbox" onChange={this.setInclude} value={this.state.includeNotPlayed}></input>
+              <label> Only include not played movies</label>
+  
+            </div>
+            <div className="row">
+              <button className="btn btn-primary" onClick={this.handleFilter}>
+                Filter
+              </button>
             </div>
             
             <div className="functionalities-table">
